f77e5558908e08edcb9ba1ae58d9cde12b520aa4
Kian Candelario <kiancandelario1@gmail.com>
2024-09-30 01:49:43 +0800

    feat: Added page for admin dashboard

---
+++ src/app/dashboard_admin/page.tsx
@@ -0,0 +1,12 @@
+import DashboardAdmin from "../components/DashboardAdmin";
+
+
+const DashboardPageAdmin = () => {
+    return ( 
+        <main className="min-h-screen h-screen flex items-center justify-center px-20 bg-no-repeat bg-cover bg-[url('/SVGs/blurry-gradient-haikei.svg')]">
+            <DashboardAdmin></DashboardAdmin>
+        </main>
+    );
+}
+ 
+export default DashboardPageAdmin;
