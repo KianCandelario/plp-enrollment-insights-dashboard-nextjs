36ae3c86fe5994b050e3de936a476482b0c9d61b
Kian Candelario <kiancandelario1@gmail.com>
2024-09-30 01:51:49 +0800

    feat: Added middleware for authentication

---
+++ src/middleware.ts
@@ -0,0 +1,8 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { getToken } from 'next-auth/jwt';
+
+export { default } from 'next-auth/middleware';
+
+export const config = {
+    matcher: ["/dashboard", "/dashboard_admin"]
+}
