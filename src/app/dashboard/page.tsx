c032020d9480d212e283385029ac404e56360e2d
Kian Candelario <kiancandelario1@gmail.com>
2024-10-07 02:13:38 +0800

    feat: Started the ui of user dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,67 @@
-import DashboardUser from "./DashboardUser";
+"use client";
+
+import Dashboard from "@/app/components/pages/Dashboard";
+import { SideBar, SideBarItem } from "@/app/components/SideBar";
+import { CombineIcon, ComputerIcon, BookIcon, BriefcaseMedicalIcon, Building2Icon, FolderCode, HotelIcon } from 'lucide-react';
+import { useState } from "react";
+
 
 const DashboardPageUser = () => {
-    return ( 
-        <main className="min-h-screen h-screen flex items-center justify-center px-20 bg-no-repeat bg-cover bg-[url('/SVGs/blurry-gradient-haikei.svg')]">
-            <DashboardUser></DashboardUser>
-        </main>
-    );
-}
- 
+  const [selectedCollege, setSelectedCollege] = useState('All Colleges');
+
+  const handleCollegeSelection = (college: string) => {
+    setSelectedCollege(college);
+  };
+
+  return (
+    <main className="w-screen min-h-screen h-screen flex">
+      <SideBar>
+        <SideBarItem 
+          icon={<CombineIcon size={20} />}
+          text="All Colleges"
+          active={selectedCollege === 'All Colleges'}
+          onClick={() => handleCollegeSelection('All Colleges')}
+        />
+        <SideBarItem 
+          icon={<ComputerIcon size={20} />}
+          text="BSCS"
+          active={selectedCollege === 'BSCS'}
+          onClick={() => handleCollegeSelection('BSCS')}
+        />
+        <SideBarItem 
+          icon={<FolderCode size={20} />}
+          text="BSIT"
+          active={selectedCollege === 'BSIT'}
+          onClick={() => handleCollegeSelection('BSIT')}
+        />
+        <SideBarItem 
+          icon={<BookIcon size={20} />}
+          text="BSED"
+          active={selectedCollege === 'BSED'}
+          onClick={() => handleCollegeSelection('BSED')}
+        />
+        <SideBarItem 
+          icon={<BriefcaseMedicalIcon size={20} />}
+          text="BSN"
+          active={selectedCollege === 'BSN'}
+          onClick={() => handleCollegeSelection('BSN')}
+        />
+        <SideBarItem 
+          icon={<Building2Icon size={20} />}
+          text="BSE"
+          active={selectedCollege === 'BSE'}
+          onClick={() => handleCollegeSelection('BSE')}
+        />
+        <SideBarItem 
+          icon={<HotelIcon size={20} />}
+          text="BSHM"
+          active={selectedCollege === 'BSHM'}
+          onClick={() => handleCollegeSelection('BSHM')}
+        />
+      </SideBar>
+      <Dashboard selectedCollege={selectedCollege} />
+    </main>
+  );
+};
+
 export default DashboardPageUser;
