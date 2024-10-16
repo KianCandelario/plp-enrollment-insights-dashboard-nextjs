5ffcf90009a249078c8560ab6574c6440e38127c
Kian Candelario <kiancandelario1@gmail.com>
2024-10-17 02:18:13 +0800

    feat: Finished the sidebar

--- src/app/components/SideBar.tsx
+++ src/app/components/SideBar.tsx
@@ -5,28 +5,27 @@ import { Button } from "@/components/ui/button"
 import { SignOutButton } from "@/app/components/buttons/SignOut";
 import { quicksand } from "../utilities/fonts";
 import { User2 } from "lucide-react"; 
-import { useState } from "react";
 
 export function SideBar({children}: any) {
-    const [expanded, setExpanded] = useState(true);
-
     return ( 
-        <div className="w-60 h-screen flex flex-col border-r min-h-screen mr-10">
+        <div className="h-screen flex flex-col border-r min-h-screen bg-gray-200">
             <div className="w-full flex justify-end py-3 pr-3">
-                <Button variant="outline" size="icon" onClick={() => setExpanded(!expanded)}>
+                <Button variant="outline" size="icon">
                     <InfoIcon className="h-5 w-5" />
                 </Button>
             </div>
             <div className="flex-1 w-full px-3">
                 <div className="w-full">
                     <h2 className="text-slate-500 text-xs font-bold mb-3">Colleges</h2>
-                    <ul className="w-full ml-2 pr-2"> {children} </ul>
+                    <ul className="w-full ml-2 pr-2 space-y-4"> {/* Added space between items */}
+                        {children}
+                    </ul>
                 </div>
             </div>
-            <div className="px-3 py-3 border-t flex flex-col justify-center">
+            <div className="px-3 py-3 border-t border-gray-300 flex flex-col justify-center">
                 <div className="flex items-center mb-2">
                     <div className="bg-slate-300 rounded-full p-2">
-                        <User2></User2>    
+                        <User2 />
                     </div>
                     
                     <div className="ml-2">
@@ -35,7 +34,7 @@ export function SideBar({children}: any) {
                     </div>
                 </div>
                 <div className="flex justify-end">
-                    <SignOutButton></SignOutButton>
+                    <SignOutButton />
                 </div>
                 
             </div>
@@ -46,7 +45,7 @@ export function SideBar({children}: any) {
 export function SideBarItem({ icon, text, active, onClick }: any) {
     return (
         <li 
-            className={`relative w-full flex items-center text-ellipsis overflow-hidden whitespace-nowrap rounded-md cursor-pointer font-medium transition-colors mb-5 p-2
+            className={`relative w-full flex items-center text-ellipsis overflow-hidden whitespace-nowrap cursor-pointer font-medium transition-colors mb-3 p-2 rounded-md
             ${
                 active
                 ? "bg-gradient-to-tr from-[#C4DAD2] to-[#D6EFD8] text-[#1A5319]"
@@ -60,5 +59,4 @@ export function SideBarItem({ icon, text, active, onClick }: any) {
     );
 }
 
- 
-export default {SideBar, SideBarItem};
+export default { SideBar, SideBarItem };
