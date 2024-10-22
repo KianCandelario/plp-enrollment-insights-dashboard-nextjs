86ffd74bc4605ed5740648a53b74cf2afd4f3f06
Kian Candelario <kiancandelario1@gmail.com>
2024-10-23 01:18:24 +0800

    feat: Completed the sidebar

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,7 +2,20 @@
 
 import Dashboard from "@/app/components/pages/Dashboard";
 import { SideBar, SideBarItem } from "@/app/components/SideBar";
-import { CombineIcon, ComputerIcon, BookIcon, BriefcaseMedicalIcon, Building2Icon, FolderCode, HotelIcon } from 'lucide-react';
+import { 
+  CombineIcon, 
+  ComputerIcon,
+  BriefcaseBusiness,
+  BookTextIcon,
+  BookXIcon, 
+  BookOpenIcon, 
+  BriefcaseMedicalIcon, 
+  FolderCode, 
+  HotelIcon, 
+  CalculatorIcon,
+  Brain,
+  CogIcon
+} from 'lucide-react';
 import { useState } from "react";
 
 const DashboardPageUser = () => {
@@ -15,7 +28,7 @@ const DashboardPageUser = () => {
   return (
     <main className="min-h-screen h-screen flex">
       {/* Sidebar with fixed position */}
-      <div className="w-64 h-screen relative">
+      <div className="w-72 h-screen relative">
         <SideBar className="sticky top-0 h-screen">
           <SideBarItem 
             icon={<CombineIcon size={20} />}
@@ -36,11 +49,43 @@ const DashboardPageUser = () => {
             onClick={() => handleCollegeSelection('BSIT')}
           />
           <SideBarItem 
-            icon={<BookIcon size={20} />}
-            text="BSED"
-            active={selectedCollege === 'BSED'}
-            onClick={() => handleCollegeSelection('BSED')}
+            icon={<CalculatorIcon size={20} />}
+            text="BSA"
+            active={selectedCollege === 'BSA'}
+            onClick={() => handleCollegeSelection('BSA')}
           />
+          <SideBarItem 
+            icon={<BriefcaseBusiness size={20} />}
+            text="BSBA"
+            active={selectedCollege === 'BSBA'}
+            onClick={() => handleCollegeSelection('BSBA')}
+          />
+
+          <SideBarItem 
+            icon={<BookOpenIcon size={20} />}
+            text="BEEd"
+            active={selectedCollege === 'BEEd'}
+            onClick={() => handleCollegeSelection('BEEd')}
+          />
+          <SideBarItem 
+            icon={<BookTextIcon size={20} />}
+            text="BSEd Major in Filipino"
+            active={selectedCollege === 'BSEd Major in Filipino'}
+            onClick={() => handleCollegeSelection('BSEd Major in Filipino')}
+          />
+          <SideBarItem 
+            icon={<BookTextIcon size={20} />}
+            text="BSEd Major in English"
+            active={selectedCollege === 'BSEd Major in English'}
+            onClick={() => handleCollegeSelection('BSEd Major in English')}
+          />
+          <SideBarItem 
+            icon={<BookXIcon size={20} />}
+            text="BSEd Major in Math"
+            active={selectedCollege === 'BSEd Major in Math'}
+            onClick={() => handleCollegeSelection('BSEd Major in Math')}
+          />
+
           <SideBarItem 
             icon={<BriefcaseMedicalIcon size={20} />}
             text="BSN"
@@ -48,16 +93,22 @@ const DashboardPageUser = () => {
             onClick={() => handleCollegeSelection('BSN')}
           />
           <SideBarItem 
-            icon={<Building2Icon size={20} />}
-            text="BSE"
-            active={selectedCollege === 'BSE'}
-            onClick={() => handleCollegeSelection('BSE')}
+            icon={<CogIcon size={20} />}
+            text="BSECE"
+            active={selectedCollege === 'BSECE'}
+            onClick={() => handleCollegeSelection('BSECE')}
           />
           <SideBarItem 
             icon={<HotelIcon size={20} />}
             text="BSHM"
             active={selectedCollege === 'BSHM'}
             onClick={() => handleCollegeSelection('BSHM')}
+          />
+          <SideBarItem 
+            icon={<Brain size={20} />}
+            text="ABPsych"
+            active={selectedCollege === 'ABPsych'}
+            onClick={() => handleCollegeSelection('ABPsych')}
           />
         </SideBar>
       </div>
