d59b430a071a8d9045386c4da4721fa814779700
Kian Candelario <kiancandelario1@gmail.com>
2024-11-06 01:39:43 +0800

    refactor: Separated the code for database connection

--- src/app/api/enrollments/route.ts
+++ src/app/api/enrollments/route.ts
@@ -1,24 +1,9 @@
 import { NextResponse } from 'next/server';
-import { Pool } from 'pg';
-
-// Create a connection pool
-const pool = new Pool({
-  // Your local PostgreSQL connection config
-  user: process.env.POSTGRES_USER,
-  host: process.env.POSTGRES_HOST || 'localhost',
-  database: process.env.POSTGRES_DATABASE,
-  password: process.env.POSTGRES_PASSWORD,
-  port: 5432,
-});
-
-// Add connection error handling
-pool.on('error', (err) => {
-  console.error('Unexpected error on idle client', err);
-  process.exit(-1);
-});
+import { getPool } from '@/lib/db';
 
 export async function GET(request: Request) {
   try {
+    const pool = getPool();
     const { searchParams } = new URL(request.url);
     let courseCode = searchParams.get('courseCode') || 'GRAND_TOTAL';
 
