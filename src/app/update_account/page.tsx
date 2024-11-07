baeaff583024168f144d423c6a8430a2d4d3beac
Kian Candelario <kiancandelario1@gmail.com>
2024-11-07 17:37:35 +0800

    feat: Added update account page

---
+++ src/app/update_account/page.tsx
@@ -0,0 +1,11 @@
+import UpdateAccount from "../components/pages/UpdateAccount";
+
+const updateAccountPage = () => {
+    return ( 
+        <main className="w-dvw h-dvh flex justify-center items-center gradient-bg">
+            <UpdateAccount></UpdateAccount>
+        </main>
+    );
+}
+ 
+export default updateAccountPage;
