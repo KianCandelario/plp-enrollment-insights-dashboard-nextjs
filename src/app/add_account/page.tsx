3833aa7caa6fafe9364d1669277a90a939220ec4
Kian Candelario <kiancandelario1@gmail.com>
2024-12-01 01:36:23 +0800

    feat: Added add account page

---
+++ src/app/add_account/page.tsx
@@ -0,0 +1,12 @@
+// app/add_account/page.tsx
+import AddAccount from '@/app/components/pages/AddAccount'
+
+const NewAccountPage = () => {
+    return ( 
+        <main className="w-dvw h-dvh flex justify-center items-center gradient-bg">
+            <AddAccount />
+        </main>
+    );
+}
+
+export default NewAccountPage;
