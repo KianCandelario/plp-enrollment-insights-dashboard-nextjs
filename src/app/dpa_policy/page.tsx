4f30bf06fb5bbe70e1f86857a9919a1aec5220c5
Kian Candelario <kiancandelario1@gmail.com>
2024-12-09 01:46:38 +0800

    feat: Added dpa policy page

---
+++ src/app/dpa_policy/page.tsx
@@ -0,0 +1,11 @@
+import DPAPolicy from "../components/pages/DPAPolicy";
+
+const dpaPolicyPage = () => {
+    return ( 
+        <main className="w-dvw h-dvh flex justify-center items-center gradient-bg">
+            <DPAPolicy></DPAPolicy>
+        </main>
+    );
+}
+ 
+export default dpaPolicyPage;
